0f6cf54a287b30c4a38af275c861db926e507410
Yrmen456 <120619041+Yrmen456@users.noreply.github.com>
2023-04-07 14:26:53 +0500

    1

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/tr', function () {
-    return view('tr');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
+    return view('form_communications');
 });
 
 Route::get('/welcome', function () {
@@ -40,13 +28,4 @@ Route::get('/form', function () {
 use App\Http\Controllers\PostBitrix24Controller;
 Route::post('/postic', [PostBitrix24Controller::class, 'Submit']);
 
-use App\Http\Controllers\MyPageController;
-Route::get('/my_page', [MyPageController::class, 'index']);
 
-Route::get('/test', function () {
-    return 'test';
-});
-
-Route::post('/submit', function (Request $request) {
-    return redirect()->back();
-});
